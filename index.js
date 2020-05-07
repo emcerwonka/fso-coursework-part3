7fbdce3a77571ca442ade2b4bf2ee5ab580dcf19
emcerwonka <evancerwonka@gmail.com>
2020-05-07 15:38:04 -0500

    newprodbuild

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
 
-app.use(express.json())
 app.use(cors())
+app.use(express.json())
+app.use(express.static('build'))
 
 morgan.token('req-body', (req, res) => {
   return JSON.stringify(req.body)
