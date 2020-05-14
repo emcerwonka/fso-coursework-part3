aeefb68659c5e1a1e3c03a88eecdca55d95deb98
emcerwonka <evancerwonka@gmail.com>
2020-05-13 21:07:35 -0500

    Completed exercise 3.22.

--- index.js
+++ index.js
@@ -61,11 +61,6 @@ app.post('/api/persons', (req, res, next) => {
 app.put('/api/persons/:id', (req, res, next) => {
   const body = req.body
 
-  const person = {
-    name: body.name,
-    number: body.number
-  }
-
   Person.findByIdAndUpdate(req.params.id, body, { new: true })
     .then(updatedPerson => {
       res.status(200).json(updatedPerson.toJSON())
